15ea2403ff12d94269e3d36d7a1063b152c77302
Austn Hughes <austen.hughes.91@gmail.com>
2021-01-30 21:18:06 -0700

    homework done - done the long way - needs a read me

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,53 +1,5 @@
 $(document).ready(function() {
 
-    $(".Btn1am").on("click", function(){
-        var textarea = $(".textarea1am").val();
-        localStorage.setItem("newTask1am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn2am").on("click", function(){
-        var textarea = $(".textarea2am").val();
-        localStorage.setItem("newTask2am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn3am").on("click", function(){
-        var textarea = $(".textarea3am").val();
-        localStorage.setItem("newTask3am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn4am").on("click", function(){
-        var textarea = $(".textarea4am").val();
-        localStorage.setItem("newTask4am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn5am").on("click", function(){
-        var textarea = $(".textarea5am").val();
-        localStorage.setItem("newTask5am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn6am").on("click", function(){
-        var textarea = $(".textarea6am").val();
-        localStorage.setItem("newTask6am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn7am").on("click", function(){
-        var textarea = $(".textarea7am").val();
-        localStorage.setItem("newTask7am" , textarea);
-        console.log(textarea)
-        });
-
-    $(".Btn8am").on("click", function(){
-        var textarea = $(".textarea8am").val();
-        localStorage.setItem("newTask8am" , textarea);
-        console.log(textarea)
-        });
-
     $(".Btn9am").on("click", function(){
         var textarea = $(".textarea9am").val();
         localStorage.setItem("newTask9am" , textarea);
@@ -95,54 +47,7 @@ $(document).ready(function() {
         localStorage.setItem("newTask4pm" , textarea);
         console.log(textarea)
         });
-    
-    $(".Btn5pm").on("click", function(){
-        var textarea = $(".textarea5pm").val();
-        localStorage.setItem("newTask5pm" , textarea);
-        console.log(textarea)
-        });
-    
-    $(".Btn6pm").on("click", function(){
-        var textarea = $(".textarea6pm").val();
-        localStorage.setItem("newTask6pm" , textarea);
-        console.log(textarea)
-        });
-    
-    $(".Btn7pm").on("click", function(){
-        var textarea = $(".textarea7pm").val();
-        localStorage.setItem("newTask7pm" , textarea);
-        console.log(textarea)
-        });
-    
-    $(".Btn8pm").on("click", function(){
-        var textarea = $(".textarea8pm").val();
-        localStorage.setItem("newTask8pm" , textarea);
-        console.log(textarea)
-        });
-    
-    $(".Btn9pm").on("click", function(){
-        var textarea = $(".textarea9pm").val();
-        localStorage.setItem("newTask9pm" , textarea);
-        console.log(textarea)
-        });
-    
-    $(".Btn10pm").on("click", function(){
-        var textarea = $(".textarea10pm").val();
-        localStorage.setItem("newTask10pm" , textarea);
-        console.log(textarea)
-        });
-    
-    $(".Btn11pm").on("click", function(){
-        var textarea = $(".textarea11pm").val();
-        localStorage.setItem("newTask11pm" , textarea);
-        console.log(textarea)
-        });
-   
-    $(".Btn12am").on("click", function(){
-        var textarea = $(".textarea12am").val();
-        localStorage.setItem("newTask12am" , textarea);
-        console.log(textarea)
-        });
+
 
     loadPage ();
     function loadPage (){
@@ -155,77 +60,130 @@ $(document).ready(function() {
         $("#currentDay").append(displayDay);
         }
 
-        checkTime ();
-        function checkTime(){
-            var getTime = moment().format("LT");
-            console.log(getTime);
-            var setTime = moment().format("LT")
-            console.log(setTime);
-
-            if (getTime === setTime){
-                console.log("time is now");
-                $(".time-block12").addClass("present");
-            } else if (getTime < setTime){
-                $(".time-block12").addClass("past")
-            } else if (getTime > setTime){
-                $(".time-block12").addClass("future") 
-            }
-
-            }
-        }
-
-      var getTask12pm = localStorage.getItem("newTask12pm");
-      var getTask11pm = localStorage.getItem("newTask11pm");
-      var getTask10pm = localStorage.getItem("newTask10pm");
-      var getTask9pm = localStorage.getItem("newTask9pm");
-      var getTask8pm = localStorage.getItem("newTask8pm");
-      var getTask7pm = localStorage.getItem("newTask7pm");
-      var getTask6pm = localStorage.getItem("newTask6pm");
       var getTask5pm = localStorage.getItem("newTask5pm");
       var getTask4pm = localStorage.getItem("newTask4pm");
       var getTask3pm = localStorage.getItem("newTask3pm");
       var getTask2pm = localStorage.getItem("newTask2pm");
       var getTask1pm = localStorage.getItem("newTask1pm");
-      var getTask12am = localStorage.getItem("newTask12am");
+      var getTask12pm = localStorage.getItem("newTask12pm");
       var getTask11am = localStorage.getItem("newTask11am");
       var getTask10am = localStorage.getItem("newTask10am");
       var getTask9am = localStorage.getItem("newTask9am");
-      var getTask8am = localStorage.getItem("newTask8am");
-      var getTask7am = localStorage.getItem("newTask7am");
-      var getTask6am = localStorage.getItem("newTask6am");
-      var getTask5am = localStorage.getItem("newTask5am");
-      var getTask4am = localStorage.getItem("newTask4am");
-      var getTask3am = localStorage.getItem("newTask3am");
-      var getTask2am = localStorage.getItem("newTask2am");
-      var getTask1am = localStorage.getItem("newTask1am");
       
+        $(".textarea9am").text(getTask9am);
+        $(".textarea10am").text(getTask10am);
+        $(".textarea11am").text(getTask11am);
         $(".textarea12pm").text(getTask12pm);
         $(".textarea1pm").text(getTask1pm);
         $(".textarea2pm").text(getTask2pm);
         $(".textarea3pm").text(getTask3pm);
         $(".textarea4pm").text(getTask4pm);
         $(".textarea5pm").text(getTask5pm);
-        $(".textarea6pm").text(getTask6pm);
-        $(".textarea7pm").text(getTask7pm);
-        $(".textarea8pm").text(getTask8pm);
-        $(".textarea9pm").text(getTask9pm);
-        $(".textarea10pm").text(getTask10pm);
-        $(".textarea11pm").text(getTask11pm);
-        $(".textarea12am").text(getTask12am);
-        $(".textarea1am").text(getTask1am);
-        $(".textarea2am").text(getTask2am);
-        $(".textarea3am").text(getTask3am);
-        $(".textarea4am").text(getTask4am);
-        $(".textarea5am").text(getTask5am);
-        $(".textarea6am").text(getTask6am);
-        $(".textarea7am").text(getTask7am);
-        $(".textarea8am").text(getTask8am);
-        $(".textarea9am").text(getTask9am);
-        $(".textarea10am").text(getTask10am);
-        $(".textarea11am").text(getTask11am);
+         
+        checkTime ();
+        function checkTime(){
+            var getTime = moment().format("LT");
+            console.log(getTime);
+            
+            var setTime9am = moment(9,"HH").format("LT");
+            console.log(setTime9am);
+
+            if (getTime === setTime9am){
+                $(".time-block9am").addClass("present");
+            } else if (getTime > setTime9am){
+                $(".time-block9am").addClass("past")
+            } else if (getTime < setTime9am){
+                $(".time-block9am").addClass("future") 
+            }
+
+            var setTime10am = moment(10,"HH").format("LT");
+            console.log(setTime10am);
+
+            if (getTime === setTime10am){
+                $(".time-block10am").addClass("present");
+            } else if (getTime > setTime10am){
+                $(".time-block10am").addClass("past")
+            } else if (getTime < setTime10am){
+                $(".time-block10am").addClass("future") 
+            }
+
+            var setTime11am = moment(11,"HH").format("LT");
+            console.log(setTime11am);
+
+            if (getTime === setTime11am){
+                $(".time-block11am").addClass("present");
+            } else if (getTime > setTime11am){
+                $(".time-block11am").addClass("past")
+            } else if (getTime < setTime11am){
+                $(".time-block11am").addClass("future") 
+            }
+
+            var setTime12pm = moment(12,"HH").format("LT");
+            console.log(setTime12pm);
+
+            if (getTime === setTime12pm){
+                $(".time-block12pm").addClass("present");
+            } else if (getTime > setTime12pm){
+                $(".time-block12pm").addClass("past")
+            } else if (getTime < setTime12pm){
+                $(".time-block12pm").addClass("future") 
+            }
+
+            var setTime1pm = moment(13,"HH").format("LT");
+            console.log(setTime1pm);
+
+            if (getTime === setTime1pm){
+                $(".time-block1pm").addClass("present");
+            } else if (getTime > setTime1pm){
+                $(".time-block1pm").addClass("past")
+            } else if (getTime < setTime1pm){
+                $(".time-block1pm").addClass("future") 
+            }
+
+            var setTime2pm = moment(14,"HH").format("LT");
+            console.log(setTime2pm);
+
+            if (getTime === setTime2pm){
+                $(".time-block2pm").addClass("present");
+            } else if (getTime > setTime2pm){
+                $(".time-block2pm").addClass("past")
+            } else if (getTime < setTime2pm){
+                $(".time-block2pm").addClass("future") 
+            }
+
+            var setTime3pm = moment(15,"HH").format("LT");
+            console.log(setTime3pm);
+
+            if (getTime === setTime3pm){
+                $(".time-block3pm").addClass("present");
+            } else if (getTime > setTime3pm){
+                $(".time-block3pm").addClass("past")
+            } else if (getTime < setTime3pm){
+                $(".time-block3pm").addClass("future") 
+            }
+
+            var setTime4pm = moment(16,"HH").format("LT");
+            console.log(setTime4pm);
+
+            if (getTime === setTime4pm){
+                $(".time-block4pm").addClass("present");
+            } else if (getTime > setTime4pm){
+                $(".time-block4pm").addClass("past")
+            } else if (getTime < setTime4pm){
+                $(".time-block4pm").addClass("future") 
+            }
+
+            var setTime5pm = moment(17,"HH").format("LT");
+            console.log(setTime5pm);
+
+            if (getTime === setTime5pm){
+                $(".time-block5pm").addClass("present");
+            } else if (getTime > setTime5pm){
+                $(".time-block5pm").addClass("past")
+            } else if (getTime < setTime5pm){
+                $(".time-block5pm").addClass("future") 
+            }
+
+            }
+        }
     })
-
-    
-    // color code time blocks based on realtime hrs
-    
-
